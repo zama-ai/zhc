7fd16f87a400d2eea94c15fa005268599f2824e4
Alexandre Péré <alexandre.pere@zama.ai>
2026-02-04 17:20:35 +0100

    feat: capture interpretation panics in annotated ir

--- hc_ir/src/annotation/traits.rs
+++ hc_ir/src/annotation/traits.rs
@@ -1,6 +1,6 @@
 use std::fmt::Debug;
 use std::hash::Hash;
 
-pub trait Annotation: PartialEq + Debug + Hash + Clone + 'static {}
+pub trait Annotation: PartialEq + Eq + Debug + Hash + Clone + 'static {}
 
 impl<T> Annotation for T where T: PartialEq + Eq + Debug + Hash + Clone + 'static {}
