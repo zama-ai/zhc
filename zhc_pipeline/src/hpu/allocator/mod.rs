28f4f582061c7182e5bb8963f5fc1f2d00e2892d
lsainati <lucas.sainati@zama.ai>
2026-07-31 16:02:46 +0200

    feat(flip): adding flip operation support

--- zhc_pipeline/src/hpu/allocator/mod.rs
+++ zhc_pipeline/src/hpu/allocator/mod.rs
@@ -27,7 +27,7 @@ pub fn allocate_registers(ir: &IR<HpuLang>, config: &HpuConfig) -> IR<DopLang> {
 mod test {
     use zhc_builder::{
         Builder, CiphertextSpec, add, adds, bitwise_and, bitwise_or, bitwise_xor, cast, cmp_gt,
-        div, if_then_else, if_then_zero, mul, overflow_ssub, overflow_subs, ssub, subs,
+        div, flip, if_then_else, if_then_zero, mul, overflow_ssub, overflow_subs, ssub, subs,
     };
     use zhc_config::hpu::{HpuConfig, PhysicalConfig};
     use zhc_ir::{IR, PrintWalker};
@@ -219,6 +219,7 @@ mod test {
             check(bitwise_xor(spec));
             check(if_then_else(spec));
             check(if_then_zero(spec));
+            check(flip(spec));
             check(mul(spec));
             check(div(spec));
             check(cast(spec, 2));
