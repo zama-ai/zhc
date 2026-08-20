8d69935ccf071bb92fb3aaae661d4596a475f86a
Alexandre Péré <alexandre.pere@zama.ai>
2026-08-20 10:59:53 +0200

    wip

--- zhc/examples/profile_pipeline.rs
+++ zhc/examples/profile_pipeline.rs
@@ -1,11 +1,19 @@
-use zhc_builder::CiphertextSpec;
-use zhc_pipeline::{Pipeline, compat::Iop};
+use zhc::prelude::BuilderExt;
+use zhc_builder::{Builder, CiphertextBlockSpec};
+use zhc_pipeline::Pipeline;
 
 fn main() {
-    let bd = Iop::Mul.to_builder(CiphertextSpec::new(64, 2, 2));
+    let bd = Builder::new(CiphertextBlockSpec(2,2));
+    let blocks = bd.ciphertext_split(bd.ciphertext_input(2));
+    let cst = bd.block_let_plaintext(3);
+    let a = bd.block_mul_plaintext(blocks[0], cst);
+    let a = bd.block_mul_plaintext(a, cst);
+    let a = bd.block_mul_plaintext(a, cst);
+    bd.ciphertext_output(bd.ciphertext_join([a], None));
+    bd.dump_noise_budget();
+
     let mut ppl = Pipeline::new()
         .with_builder(bd)
         .with_hpu_config(Default::default());
     ppl.get_hpu_stream();
-    ppl.draw_state().open().unwrap();
 }
