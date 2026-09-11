5a45f0f09aa2da1a8c275cdaf6e1316b1f29a020
Baptiste Roux <baptiste.roux@zama.ai>
2026-09-11 13:21:24 +0000

    feat(cli): add custom dop sample files

---
+++ zhc_cli/dop_samples/integer_w_8/cust_17_v0.asm
@@ -0,0 +1,23 @@
+; CUST_HPU0_17
+; Simple IOp to check PBS behavior
+; Dest <- PBSNone(Src_a)
+; ------------------------------------------------------------------------------
+; !preamble {
+; [signature]
+; Ciphertext<8, 2, 2> -> Ciphertext<8, 2, 2>
+; [lut]
+; None: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+; } ----------------------------------------------------------------------------
+
+LD    R0   TS[0].0
+PBS   R0   R0    PbsNone
+ST    TD[0].0 R0 
+LD    R1   TS[0].1
+PBS   R1   R1    PbsNone
+ST    TD[0].1 R1 
+LD    R2   TS[0].2
+PBS   R2   R2    PbsNone
+ST    TD[0].2 R2 
+LD    R3   TS[0].3
+PBS_F R3   R3    PbsNone
+ST    TD[0].3 R3 
---
+++ zhc_cli/dop_samples/integer_w_8/cust_3_v0.asm
@@ -0,0 +1,24 @@
+; CUST_HPU0_3
+; Simple IOp to check isc behavior
+; Generate obvious deps and check that isc correctly issued the dop
+; Correct result must bu Dest <- Src[0]
+; ------------------------------------------------------------------------------
+; !preamble {
+; [signature]
+; Ciphertext<8, 2, 2> -> Ciphertext<8, 2, 2>
+; [lut]
+; None: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+; } ----------------------------------------------------------------------------
+
+LD R0   TS[0].0
+LD R1   TS[0].1
+LD R2   TS[0].2
+LD R3   TS[0].3
+PBS R4 R0 PbsNone
+ST TD[0].0 R4 
+PBS R4 R1 PbsNone
+ST TD[0].1 R4 
+PBS R4 R2 PbsNone
+ST TD[0].2 R4 
+PBS_F R4 R3 PbsNone
+ST TD[0].3 R4 
